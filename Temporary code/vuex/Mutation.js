badf4e9efb9e5774d4d08bacc65cc0e2b608508d
toxicfy <15927630261@163.com>
2018-12-06 15:05:54 +0800

    update temporary code

--- Temporary code/vuex/Mutation.js
+++ Temporary code/vuex/Mutation.js
@@ -1,13 +1,45 @@
-// similar as event(eventType,callback(state));
+// similar as event(eventType,callback(state,payload));
 
+
+//mutation-types.js
+export const INCREMENT = 'INCREMENT';
+export const ADD = 'ADD';
+
+
+//store.js
+import Vuex from 'vuex';
+import {INCREMENT, ADD} from './mutation-types';
 
 const store = new Vuex.store({
     store: {count: 0},
     mutations: {
-        increment: state => state.count++ // state change
+        [INCREMENT]: state => state.count++,// state change
+        [ADD](state, payload) {
+            return state.count + payload.amount
+        }
     }
 });
 
 
+// NOTICE: 如果在实例创建之后添加新的属性到实例上，它不会触发视图更新, 对于mutations也是如此,所以最好建议初始化就完成所有属性的添加
+Vue.set(obj, 'newName', 123);//add
+state.obj = {...state.obj, newName: 123}; // replace obj
+
 // but we can't directly call a mutation callback(handler); need commit this mutation;
 store.commit('increment');
+// store.commit('add', {amount: 10});
+store.commit({type: "add", amount: 10}); // type property is identified as mutations type;
+
+import {mapMutations} from 'vuex';
+
+export default {
+    methods: {
+        ...mapMutations([
+            'increment' // map this.increment to this.$store.commit('increment')
+        ])
+    }
+}
+
+
+// question：why all mutation must be synchronous？
+// 因为回调函数中有异步函数，触发mutation后调用回调函数改变state的时机不能追踪；
---
+++ Temporary code/vuex/action.js
@@ -0,0 +1,42 @@
+import Vuex from 'vuex';
+import {INCREMENT} from './mutation-type';
+
+const store = new Vuex.store({
+    state: {count: 0},
+    mutation: {
+        [INCREMENT](state, payload) {
+            return state.count + payload.amount
+        }
+    },
+    action: {
+        [INCREMENT]({commit}) {
+            return new Promise((resolve, reject) => {
+                setTimeout((res) => {
+                    commit(INCREMENT, res);
+                    resolve()
+                }, 100)
+            })
+
+        }
+    }
+});
+
+//component use
+import {INCREMENT} from './mutation-type';
+
+this.$store.dispatch(INCREMENT);
+//mapAction
+export default {
+    computed: {
+        local() {
+            this[INCREMENT]().then(res => {
+                console.log(res);
+            });
+        }
+    },
+    methods: {
+        ...mapAction([
+            INCREMENT  //this[INCREMENT] === this.$store.dispatch(INCREMENT)
+        ]),
+    }
+}
