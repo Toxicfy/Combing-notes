37f5ef3cb4f6d3da5ebe8f9eef186af91bfe569c
Toxicfy <15927630261@163.com>
2019-01-10 01:34:49 +0800

    add koa

---
+++ Koa/app.js
@@ -0,0 +1,30 @@
+const Koa = require('koa');
+const json = require('koa-json');
+const mysql = require('mysql');
+const router = require('koa-router')();
+const app = new Koa();
+
+app.use(json());
+
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'test'
+})
+
+router.get('/search', async (ctx) => {
+    const searchSql = `SELECT * FROM user`;
+    connection.query(searchSql, (err, res) => {
+        if(err) throw err;
+        ctx.body = res;
+    })
+
+})
+
+// 加载路由中间件
+app.use(router.routes()).use(router.allowedMethods())
+app.listen(8000, () => {
+    console.log("server run at localhost:8000")
+});
